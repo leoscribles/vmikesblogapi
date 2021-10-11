bed831fbb998278db0d306f3f40733fe89ffb7f2
Leonard Uyanwanne <leoscribles@gmail.com>
2021-10-11 04:28:15 +0100

    modificate

--- apiconfig/settings.py
+++ apiconfig/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-olc670hh@o4bv!zyx=dv$r_qlb9j84nof4ui_jm=s9ohwcw5t6'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
